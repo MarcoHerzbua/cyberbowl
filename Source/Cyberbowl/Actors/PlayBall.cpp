b3204cdb4e588d74368ca358c88286a8d9101b91
Marco Herzog <marco.herzog@outlook.at>
2020-02-03 12:15:35 +0100

    added ball blueprint with custom collision preset; tweaked char movement

---
+++ Source/Cyberbowl/Actors/PlayBall.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayBall.h"
+
+// Sets default values
+APlayBall::APlayBall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BallStaticMesh = FindComponentByClass<UStaticMeshComponent>();
+	auto test = GetComponents();
+	if (!BallStaticMesh)
+	{
+		return;
+	}
+	BallStaticMesh->SetWorldScale3D(FVector(ScaleModifier));
+}
+
+// Called every frame
+void APlayBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
