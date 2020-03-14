64649e7495d867d84827d97b27eeb20662316915
Kerstin Hofer <khofer.mmt-b201>
2020-03-14 15:25:16 +0100

    Rewrite GameInstance in C++ an alter blueprints according to that.

--- Source/Cyberbowl/Public/PlayerController/ThirdPersonPlayerController.h
+++ Source/Cyberbowl/Public/PlayerController/ThirdPersonPlayerController.h
@@ -9,31 +9,6 @@
 /**
  * 
  */
-UENUM(BlueprintType)
-enum class ECBCharacterType : uint8
-{
-	CBCHRACTERTYPE_FIRE UMETA(DisplayName = "Fire"),
-	CBCHRACTERTYPE_EARTH UMETA(DisplayName = "Earth"),
-	CBCHRACTERTYPE_ICE UMETA(DisplayName = "Ice"),
-	CBCHRACTERTYPE_AIR UMETA(DisplayName = "Air"),
-};
-
-USTRUCT(BlueprintType)
-struct FPlayerInfo
-{
-	GENERATED_BODY()
-
-	UPROPERTY(BlueprintReadWrite)
-	int Team;
-	UPROPERTY(BlueprintReadWrite)
-	ECBCharacterType CharacterType;
-
-	FPlayerInfo()
-	{
-		Team = 0;
-		CharacterType = ECBCharacterType::CBCHRACTERTYPE_FIRE;
-	}
-};
 
 UCLASS()
 class CYBERBOWL_API AThirdPersonPlayerController : public APlayerController
