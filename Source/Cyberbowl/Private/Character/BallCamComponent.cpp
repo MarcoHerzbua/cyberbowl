71e90e39ee5a7dfddef986cfbccf4f6a06d3e371
Daniel Brantner <dbrantner.mmt-b2017@fh-salzburg.ac.at>
2020-04-13 12:24:14 +0200

    refactor: Remove gamemode dependency from BallCamComponent

--- Source/Cyberbowl/Private/Character/BallCamComponent.cpp
+++ Source/Cyberbowl/Private/Character/BallCamComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Character/BallCamComponent.h"
 
+
+#include "Actors/PlayBall.h"
 #include "Character/CyberbowlCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -21,7 +23,7 @@ void UBallCamComponent::BeginPlay()
 	Super::BeginPlay();
 
 	CameraBoom = Cast<ACyberbowlCharacter>(GetOwner())->GetCameraBoom();
-	PlayBall = Cast<UStaticMeshComponent>(Cast<AInGameGameMode>(UGameplayStatics::GetGameMode(this))->Ball->GetComponentByClass(UStaticMeshComponent::StaticClass()));
+	PlayBall = Cast<APlayBall>(UGameplayStatics::GetActorOfClass(this, APlayBall::StaticClass()))->BallStaticMesh;
 }
 
 void UBallCamComponent::FocusBall(float deltaTime)
